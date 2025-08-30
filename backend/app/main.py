a864a6c9560f4f5823ac674288c2d80e3f0ad884
Swayam Singh <2k22.csai.2212577@gmail.com>
2025-08-30 14:48:10 +0530

    updated cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,15 +13,16 @@ app = FastAPI(title="Doc Summary Assistant")
 # Allow CORS from frontend (development)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://doc-summary-assistant-eight.vercel.app/",
-        "https://doc-summary-assistant-swayam-singhs-projects.vercel.app/",
-        "https://doc-summary-assistant-jqls6p7qk-swayam-singhs-projects.vercel.app/"
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    # allow_origins=[
+    #     "https://doc-summary-assistant-eight.vercel.app/",
+    #     "https://doc-summary-assistant-swayam-singhs-projects.vercel.app/",
+    #     "https://doc-summary-assistant-jqls6p7qk-swayam-singhs-projects.vercel.app/",
+    #     "http://localhost:3000",
+    #     "http://127.0.0.1:3000",
+    #     "http://localhost:5173",
+    #     "http://127.0.0.1:5173"
+    # ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
